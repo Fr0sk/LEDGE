91fb2c1e3f96210e1a7808bb4fb68e8cba4f8567
Pāvels Nadtočajevs <7645683+bruvzg@users.noreply.github.com>
2026-08-10 15:30:12 +0300

    [WinRT, 4.7] Add exception handling to `DispatcherQueueOptions` init.

--- platform/windows/display_server_windows.cpp
+++ platform/windows/display_server_windows.cpp
@@ -7925,6 +7925,13 @@ DisplayServerWindows::DisplayServerWindows(const String &p_rendering_driver, Dis
 		FreeLibrary(nt_lib);
 	}
 
+	if (os_ver.dwBuildNumber >= 16299) { // Windows 10 Redstone 3 (1709)+ only.
+		has_winrt_queue = WinRTUtils::create_queue();
+	}
+
+	// Enforce default keep screen on value.
+	screen_set_keep_on(GLOBAL_GET("display/window/energy_saving/keep_screen_on"));
+
 	// Load UXTheme.
 	if (os_ver.dwBuildNumber >= 10240) { // Not available on Wine, use only if real Windows 10/11 detected.
 		HMODULE ux_theme_lib = LoadLibraryW(L"uxtheme.dll");
